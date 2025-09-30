8d5c8b7a1588763be871acfb8193c9cd8a3522ad
Victor Toupitzen Specian <victor.specian@kobel.com.br>
2025-09-30 09:27:43 -0300

    add Header

---
+++ src/components/Header.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <section className="text-center pt-16 pb-24">
+            <p className="text-red-700 font-semibold mb-3">
+                Sobre nós
+            </p>
+
+            <h1 className='text-5xl font-extrabold text-gray-900 mb-6'>
+                Por que somos diferentes?
+            </h1>
+
+            <p className='text-xl text-gray-500 max-w-4xl mx-auto'>
+                Nós focamos nos detalhes de tudo que fazemos. Tudo para ajudar as empresas de todo o mundo a se concentrarem naquilo que é realmente importante para elas.
+            </p>
+        </section>
+    )
+}
+
+export default Header
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -101,7 +101,7 @@ const Navbar = () => {
                             height={logoHeight}
                         />
                     </Link>
-                    <button onClick={() => setIsMenuOpen(false)}>
+                    <button onClick={() => setIsMenuOpen(false)} className="cursor-pointer">
                         
                         <svg className="w-6 h-6 text-gray-900" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12"></path>
